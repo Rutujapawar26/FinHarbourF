be57abd61cdecac23a6954af3fe71df2834ae9d1
Sumaira Mulla <sumairamulla07@gmail.com>
2026-04-15 19:37:11 +0530

    Change MongoDB connection to use process.env.MONGO_URI
    Updated MongoDB connection to use environment variable for URI.

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ app.use(express.json());
 /* =========================
    DATABASE CONNECTION
 ========================= */
-mongoose.connect('mongodb://127.0.0.1:27017/fintech_db')
+mongoose.connect(process.env.MONGO_URI)
     .then(() => console.log("Connected to MongoDB ✅"))
     .catch(err => console.error("MongoDB Connection Error ❌", err));
 
